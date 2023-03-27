b96091e6fca3dda0992c76b8371d8f534112f874
Michael Mena <michael.mena2408@gmail.com>
2023-03-27 17:03:44 -0300

    update

--- enviroment.ts
+++ enviroment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   baseURL: 'https://portfolio-michael-mena.koyeb.app',
-}g
+}
 
 // 'https://portfolio-michael-mena.koyeb.app'
 // 'http://localhost:5000' FakeBackend
