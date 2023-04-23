80fc1bf09bbeb00efc7ffb216b496e2e6fbb939f
Michael Mena <michael.mena2408@gmail.com>
2023-04-23 20:57:28 -0300

    update

--- enviroment.ts
+++ enviroment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://portfolio-michael-mena.koyeb.app',
 }
 
 // 'https://portfolio-michael-mena.koyeb.app'
